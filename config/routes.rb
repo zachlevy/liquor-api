dbbd8cb631dd468c76c4263c4a54ac40656e0a2a
Zachary Levy <zacharyalevy@gmail.com>
2015-09-18 18:12:04 -0400

    product scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products, except: [:new, :edit]
   resources :stores, except: [:new, :edit]
   resources :store_classes, except: [:new, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150918214138) do
+ActiveRecord::Schema.define(version: 20150918221144) do
+
+  create_table "products", force: :cascade do |t|
+    t.integer  "number"
+    t.string   "name"
+    t.integer  "case_size"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "store_classes", force: :cascade do |t|
     t.string   "name"
