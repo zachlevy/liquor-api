72cfbbdb3c71c1ed1d650f2aff2b1659578240e0
Zachary Levy <zacharyalevy@gmail.com>
2015-09-19 12:37:50 -0400

    inventory calc sales and shipment

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -6,4 +6,25 @@ class Inventory < ActiveRecord::Base
   validates :store_id, presence: true
   validates :quantity, presence: true
   validates :lcbo_updated_on, presence: true
+
+  after_update :calc_sales
+
+  # gets the last inventory before the current inventory
+  # days ago offsets
+  def previous days_ago=0
+    Inventory.where(product: product, store: store).where("lcbo_updated_on < ?", (lcbo_updated_on - days_ago)).order(lcbo_updated_on: :desc).first
+  end
+
+  # calculates sales and shipments
+  def calc_sales
+    historic = self.previous
+    return if historic.nil?
+    sales = nil
+    shipment = 0 # units, not cases
+    while sales.nil? or sales < 0
+      sales = historic.quantity - quantity + shipment
+      shipment += product.case_size unless sales >= 0
+    end
+    self.update_attributes(:sales => sales, :shipment => shipment)
+  end
 end
